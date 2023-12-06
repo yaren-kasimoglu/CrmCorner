c788174a55690aafa33d9a6dbbf983fc4fd21eb4
yaren-kasimoglu <yaren.kkasimoglu@gmail.com>
2023-12-06 22:22:39 +0300

    TaskController eklendi

---
+++ CrmCorner/Controllers/TaskController.cs
@@ -0,0 +1,18 @@
+﻿using CrmCorner.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrmCorner.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly CrmcornerContext _context;
+        public TaskController(CrmcornerContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
