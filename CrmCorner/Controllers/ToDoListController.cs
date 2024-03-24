c3706f1fee86cf5cb5edc9a30d94f2612dcf4a72
yazilimcikizlar <yazilimcikizlar@hotmail.com>
2024-03-24 05:48:54 +0300

    ToDolist ekranında eklemeler yapıldı.

--- CrmCorner/Controllers/ToDoListController.cs
+++ CrmCorner/Controllers/ToDoListController.cs
@@ -3,6 +3,7 @@ using CrmCorner.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Exchange.WebServices.Data;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Graph;
 
@@ -19,53 +20,103 @@ namespace CrmCorner.Controllers
         }
         public async Task<IActionResult> ToDoList()
         {
+            
             var currentUser = await _userManager.GetUserAsync(User);
-            ToDoListDay(currentUser);
-            return View();
-
+            ToDoListDay();
+            var todoList = _context.ToDoList
+             .Where(e => e.UserId == currentUser.Id)
+             .ToList();
+             ViewBag.ToDoList = todoList;
+             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> ToDoListAdd(string selected, string unselected,string maingoals,string title)
+        public async Task<IActionResult> ToDoListAdd(string selected, string unselected,string maingoals,string title, int itemId)
         {
             var currentUser = await _userManager.GetUserAsync(User);
             var today = DateTime.Today;
-            var toDoValue = _context.ToDos
-           .Include(e => e.AppUser)
-           .Where(e => e.UserId == currentUser.Id && e.SystemDate==today)
-           .Select(e => e.Id).FirstOrDefault();
-            ToDo toDo = new ToDo();
-            toDo.SystemDate = DateTime.Today;
-            toDo.DoneList = selected;
-            toDo.NotDoneList = unselected;
-            toDo.UserId = currentUser.Id;
-            toDo.MainGoalTitle = maingoals;
-            toDo.Title = title;
-            if (currentUser != null)
+            if (itemId ==0)
             {
-                if (toDoValue == 0)
+                var toDoValue = _context.ToDos
+              .Include(e => e.AppUser)
+              .Where(e => e.UserId == currentUser.Id && e.SystemDate == today)
+              .Select(e => e.Id).FirstOrDefault();
+                ToDo toDo = new ToDo();
+                toDo.SystemDate = DateTime.Today;
+                toDo.DoneList = selected;
+                toDo.NotDoneList = unselected;
+                toDo.UserId = currentUser.Id;
+                toDo.MainGoalTitle = maingoals;
+                toDo.Title = title;
+                if (currentUser != null)
                 {
-                     _context.ToDos.Add(toDo);
-                     _context.SaveChanges();
+                    if (toDoValue == 0 && (selected != null || unselected != null))
+                    {
+                        _context.ToDos.Add(toDo);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        toDo.Id = toDoValue;
+                        _context.ToDos.Update(toDo);
+                        _context.SaveChanges();
+                    }
                 }
-                else
+            }
+            else
+            {
+                var toDoValue = _context.ToDoList
+               .Where(e => e.UserId == currentUser.Id && e.Title == title)
+               .Select(e => e.Id).FirstOrDefault();
+                ToDoList toDo = new ToDoList();
+                toDo.SystemDate = DateTime.Today;
+                toDo.DoneList = selected;
+                toDo.NotDoneList = unselected;
+                toDo.UserId = currentUser.Id;
+                toDo.MainGoalTitle = maingoals;
+                toDo.Title = title;
+                if (currentUser != null)
                 {
-                    toDo.Id = toDoValue;
-                    _context.ToDos.Update(toDo);
-                    _context.SaveChanges();
+                    if (toDoValue == 0 && (selected != null || unselected != null))
+                    {
+                        _context.ToDoList.Add(toDo);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        toDo.Id = toDoValue;
+                        _context.ToDoList.Update(toDo);
+                        _context.SaveChanges();
+                    }
                 }
             }
            return Json(new { Message = "success" });
-            
-         
         }
         [HttpPost]
-        public async Task<IActionResult> ToDoListDay(AppUser currentUser)
+        public async Task<IActionResult> ToDoListAddList(string title)
         {
-            if (currentUser.CompanyId == 0)
+            var currentUser = await _userManager.GetUserAsync(User);
+            ToDoList toDolist = new ToDoList();
+            toDolist.SystemDate = DateTime.Today;
+            toDolist.UserId = currentUser.Id;
+            toDolist.Title = title;
+            try
             {
-                currentUser = await _userManager.GetUserAsync(User);
+                _context.ToDoList.Add(toDolist);
+                _context.SaveChanges();
             }
+            catch(Exception ex)
+            {
+           
+            }
+            return Json(new { Message = "success" });
+        }
+            
+        [HttpPost]
+        public async Task<IActionResult> ToDoListDay()
+        {
+            
+            var currentUser = await _userManager.GetUserAsync(User);
             var today = DateTime.Today;
             string[] dataArray;
             string[] dataArrays;
@@ -92,7 +143,14 @@ namespace CrmCorner.Controllers
                     ViewBag.MainGoalTitle = todo.FirstOrDefault(e => e.UserId == currentUser.Id)?.MainGoalTitle;
                 }
                 //seçili olanları ve seçili olmayanları ekle
-                return View(todo);
+                var jsonData = new
+                {
+                    MainGoalTitle = ViewBag.MainGoalTitle,
+                    Title = ViewBag.TitleValue,
+                    TaskData = ViewBag.TaskData,
+                    NotTaskData = ViewBag.NotTaskData
+                };
+                return Json(new { Message = jsonData });
             }
             else
             {
@@ -100,6 +158,52 @@ namespace CrmCorner.Controllers
                 return View();
             }
         }
+        [HttpPost]
+        public async Task<IActionResult> GetToDoList(int id)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if(currentUser != null)
+            {
+                var today = DateTime.Today;
+                string[] dataArray;
+                string[] dataArrays;
+                var todo = _context.ToDoList
+                .Where(e => e.UserId == currentUser.Id && e.Id == id)
+                .ToList();
+                if (todo.Count > 0)
+                {
+                    var selected = todo.FirstOrDefault(e => e.UserId == currentUser.Id)?.DoneList;
+                    var unselected = todo.FirstOrDefault(e => e.UserId == currentUser.Id)?.NotDoneList;
+                    if (selected != null)
+                    {
+                        dataArray = selected.Split(',');
+                        ViewBag.TaskData = dataArray;
+                    }
+                    if (unselected != null)
+                    {
+                        dataArrays = unselected.Split(',');
+                        ViewBag.NotTaskData = dataArrays;
+                    }
+                    ViewBag.TitleValue = todo.FirstOrDefault(e => e.UserId == currentUser.Id)?.Title;
+                    ViewBag.MainGoalTitle = todo.FirstOrDefault(e => e.UserId == currentUser.Id)?.MainGoalTitle;
+                }
+                var jsonData = new
+                {
+                    MainGoalTitle = ViewBag.MainGoalTitle,
+                    Title = ViewBag.TitleValue,
+                    TaskData = ViewBag.TaskData,
+                    NotTaskData = ViewBag.NotTaskData
+                };
+                return Json(new { Message = jsonData });
+
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Geçerli kullanıcı bilgisi bulunamadı.";
+                return View();
+            }
+        }
+        
     }
         
 }
