f1189291b605490840164acd9af3daa3fdf588c2
yazilimcikizlar <yazilimcikizlar@hotmail.com>
2024-09-21 17:03:45 +0300

    Email app için hazırlıklar-1

---
+++ CrmCorner/Controllers/EmailListController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CrmCorner.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrmCorner.Controllers
+{
+	public class EmailListController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly CrmCornerContext _context;
+        public EmailListController(CrmCornerContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> GetEmailList()
+		{
+            return View("EmailList");
+        }
+    }
+}
+
---
+++ CrmCorner/Models/EmailList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CrmCorner.Models
+{
+	public class EmailList
+	{
+		
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string SendMail { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Boolean IsStar { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string CC { get; set; }
+
+    }
+}
+
